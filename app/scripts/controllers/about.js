e2e8894f323fb0c524d1390a0c1149430e6702a8
Jeel Shah <jeelshah96@gmail.com>
2015-08-19 09:52:03 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nobsjwApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nobsjwApp
+ */
+angular.module('nobsjwApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nobsjwApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nobsjwApp
+ */
+angular.module('nobsjwApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
